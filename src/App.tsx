f13ef80f2e579e0548918983a3d28f463eb94d6c
vectuz254 <quintineluke16@gmail.com>
2026-08-19 10:38:10 +0300

    Add CtaFooter component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,12 @@
+import CtaFooter from "./components/CtaFooter";
 
+function App() {
+  return (
+    <main className="min-h-screen bg-black text-white flex flex-col justify-end">
+      {/* CTA + Footer Section */}
+      <CtaFooter />
+    </main>
+  );
+}
+
+export default App;
